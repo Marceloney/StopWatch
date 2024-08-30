80d1be71bdddda93f6e5aec270e22c723f04213e
MarceloNey <marcelodevjr@gmail.com>
2024-08-30 04:43:37 -0300

    Foi finalizado o Menu e também criado um PreStart

--- Program.cs
+++ Program.cs
@@ -1,26 +1,70 @@
 ﻿using System;
+using System.Data;
 
 
 namespace StopWatch
 {
     class Program
     {
-        static void Main (string[] agrs)
-        {   
-            Console.Clear();
-            Star(6);
-        
+        static void Main(string[] agrs)
+        {
+
+            Menu();
+
 
         }
 
-        static void Star(int time)
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("S = Segundos ==> 10s = 10 Segundos");
+            Console.WriteLine("M = Minutos ==> 1m = 1 Minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar? ");
+            Console.WriteLine("---------------------------------------");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if (type == 'm')
+            {
+                multiplier = 60;
+            }
+            if (time == 0)
+            {
+                System.Environment.Exit(0);
+            }
+
+            PreStart(time * multiplier);
+
+
+
+
+        }
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go!!!");
+            Thread.Sleep(2500);
+
+            Start(time);
+        
+        }
+        static void Start(int time)
         {
             Console.Clear();
 
             int currentTime = 0;
-            
-            while(currentTime != time)
-            {   
+
+            while (currentTime != time)
+            {
                 Console.Clear();
                 Console.WriteLine();
                 currentTime++;
@@ -31,6 +75,7 @@ namespace StopWatch
             Console.Clear();
             System.Console.WriteLine("StopWatch finalizado!");
             Thread.Sleep(2500);
+            Menu();
         }
     }
 }
