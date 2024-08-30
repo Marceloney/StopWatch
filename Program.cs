1b6d0cf5b6c192fa88a65c73d9b29b620c125117
MarceloNey <marcelodevjr@gmail.com>
2024-08-30 03:51:48 -0300

    criando o Menu

--- Program.cs
+++ Program.cs
@@ -1,14 +1,36 @@
 ﻿using System;
 
+
 namespace StopWatch
 {
     class Program
     {
         static void Main (string[] agrs)
+        {   
+            Console.Clear();
+            Star(6);
+        
+
+        }
+
+        static void Star(int time)
         {
             Console.Clear();
 
+            int currentTime = 0;
             
+            while(currentTime != time)
+            {   
+                Console.Clear();
+                Console.WriteLine();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            System.Console.WriteLine("StopWatch finalizado!");
+            Thread.Sleep(2500);
         }
     }
 }
